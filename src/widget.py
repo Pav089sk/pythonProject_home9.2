1ad47f937c2060bb2db0aab12eb58ea30979add0
PavelShalev <pav089sk@gmail.com>
2026-03-09 21:55:08 +0300

    result modifed widget

--- src/widget.py
+++ src/widget.py
@@ -1,11 +1,11 @@
 from masks import get_mask_card_number, get_mask_account
 
-def mask_account_card (card_or_invoice_num: str) -> str:
-    """ функция обработки информации о картах и счетах
-    """
-    parts = card_or_invoice_num.split()
-    number = parts[-1]
-    name = " ".join(parts[:-1])
+
+def mask_account_card(card_or_invoice_num: str) -> str:
+    """функция обработки информации о картах и счетах"""
+    parts: list = list(card_or_invoice_num.split())
+    number: int = int(parts[-1])
+    name: str = str(" ".join(parts[:-1]))
 
     if name.lower() == "счет":
         mask_number = get_mask_account(number)
@@ -15,11 +15,19 @@ def mask_account_card (card_or_invoice_num: str) -> str:
     return f"{name} {mask_number}"
 
 
+print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
+
 
 def get_date(date_string: str) -> str:
-    """ функция форматирования типа строки даты
-    """
+    """функция форматирования типа строки даты"""
     return f"{date_string[8:10]}-{date_string[5:7]}-{date_string[:4]}"
+
 
 print(get_date("2024-03-11T02:26:18.671407"))
