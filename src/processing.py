35c727b8909107bd368dfe066b0ea4b5c8232e36
PavelShalev <pav089sk@gmail.com>
2026-03-13 22:34:14 +0300

    realized func sort by date in processing

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_dictionary, state='EXECUTED'):
+def filter_by_state(list_dictionary: list, state = 'EXECUTED') -> list:
     """Функция возвращает новый список словарей,
     содержащий только те словари, у которых ключ
     соответствует указанному значению.
@@ -6,7 +6,7 @@ def filter_by_state(list_dictionary, state='EXECUTED'):
     sort_list=[]
     for param in list_dictionary:
          i = param.get('state')
-         if i == 'EXECUTED':
+         if i == state:
             sort_list.append(param)
 
 
@@ -16,20 +16,21 @@ print(filter_by_state([
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+]
+))
+
+
+
+def sort_by_date(list_dict: list, direction=True) -> list:
+    """Функция возвращает новый список, отсортированный по дате"""
+
+    return sorted(list_dict, key=lambda i: i.get('date'), reverse=direction)
+
+
+print(sort_by_date([
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]))
-
-
-
-# def sort_by_date(list_dict):
-#     """Функция возвращает новый список, отсортированный по дате"""
-#
-#     pass
-#
-
-# #print(filter_by_state([
-#     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-# ]))
