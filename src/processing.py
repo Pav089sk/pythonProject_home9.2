0ae8a2b01ecd651a52bd3c4c39c6e07d537b711e
PavelShalev <pav089sk@gmail.com>
2026-03-14 18:47:47 +0300

    deleting a variable and executable code

--- src/processing.py
+++ src/processing.py
@@ -5,38 +5,14 @@ def filter_by_state(list_dictionary: list, state: str ="EXECUTED") -> list:
     """
     sort_list = []
     for param in list_dictionary:
-        i = param.get("state")
-        if i == state:
+        if param.get("state") == state:
             sort_list.append(param)
 
     return sort_list
 
-
-print(
-    filter_by_state(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
 
 
 def sort_by_date(list_dict: list, direction: bool =True) -> list:
     """Функция возвращает новый список, отсортированный по дате"""
 
     return sorted(list_dict, key=lambda i: i.get("date"), reverse=direction)
-
-
-print(
-    sort_by_date(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
