5692873b72f64b91aaf88b844663ae16a1e4d55e
PavelShalev <pav089sk@gmail.com>
2026-04-17 22:53:02 +0300

    add log functional for utils

--- src/utils.py
+++ src/utils.py
@@ -2,19 +2,30 @@ import logging
 
 import json
 
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('../logs/utils.log', mode = 'w')
+file_formater = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: - %(message)s')
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
 
 def transaction_data(path: str) -> list:
     """Функция принимает путь к JSON файлу и выводит его содержимое"""
+    logger.debug('Получение пути к файлу JSON')
     try:
         with open(path) as f:
             operations = json.load(f)
             if isinstance(operations, list):
+                logger.info('Файл успешно распознан как JSON')
                 return operations
             else:
+                logger.warning('Данные не являются - JSON')
                 return []
     except FileNotFoundError:
+        logger.error('Файл по заданному пути не найден')
         return []
     except json.JSONDecodeError:
+        logger.error('Ошибочная структура данных в файле')
         return []
 
 
